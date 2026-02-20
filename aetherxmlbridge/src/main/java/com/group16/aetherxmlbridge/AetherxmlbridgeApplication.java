26db75f478016f2c68662c169db4b432e0f86d58
mbuiiiz <ductai2068@gmail.com>
2026-02-19 16:50:46 -0800

    initialized springboot project

---
+++ aetherxmlbridge/src/main/java/com/group16/aetherxmlbridge/AetherxmlbridgeApplication.java
@@ -0,0 +1,13 @@
+package com.group16.aetherxmlbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AetherxmlbridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AetherxmlbridgeApplication.class, args);
+	}
+
+}
