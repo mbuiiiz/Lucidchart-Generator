01fca1f1cfbda0e89a74763c43811f61eb7403ab
mbuiiiz <ductai2068@gmail.com>
2026-02-21 01:15:31 -0800

    fixed config

--- aetherxmlbridge/src/main/java/com/group16/aetherxmlbridge/config/SecurityConfig.java
+++ aetherxmlbridge/src/main/java/com/group16/aetherxmlbridge/config/SecurityConfig.java
@@ -3,11 +3,12 @@
     
     FOR FRONTEND UI 
     to work on every page run THIS to gain access to every page
-    docker run -p 8080:8080 aether-bridge-local
+    docker run -p 8080:8080 -e SPRING_PROFILES_ACTIVE=dev aether-bridge-local
+    
 
     FOR OAUTH SECURITY
     to work on security run THIS so you will be locked out of the program
-    docker run -p 8080:8080 -e SPRING_PROFILES_ACTIVE=dev aether-bridge-local
+    docker run -p 8080:8080 aether-bridge-local
 */
 package com.group16.aetherxmlbridge.config;
 
